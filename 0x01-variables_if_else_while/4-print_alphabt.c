cfff048935f3b9987abf5306ca130e3e6b4f5360
hb4y <hugobaymon@gmail.com>
2019-09-19 18:42:59 +0000

    all letters but...

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * Return: 0
+ * Description:  n is positive, negative or zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
