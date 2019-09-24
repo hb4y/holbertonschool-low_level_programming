e5b3677fb5bf562e4a9ca40c4a59d4add1e31ddb
hb4y <hugobaymon@gmail.com>
2019-09-24 23:29:48 +0000

    test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,36 +1,46 @@
 #include "holberton.h"
 
 /**
- * times_table - function
+ * print_times_table - function
+ * @n: table n
  *
  * Return: void
  * Print all hours.
  */
-void times_table(int n)
+void print_times_table(int n)
 {
 	int a;
 	int b;
 	int res;
 
-	if (n <= 15 || n >= 0)
+	if (n <= 15 && n >= 0)
 	{
-	for (a = 0; a <= 15; a++)
+	for (a = 0; a <= n; a++)
 	{
-		for (b = 0; b <= 15; b++)
+		for (b = 0; b <= n; b++)
 		{
 			res = a * b;
 
-			if (res > 9 && )
+			if (res >= 100)
+			{
+				_putchar(((res / 100) % 10) + '0');
+				_putchar(((res / 10) % 10) + '0');
+			}
+			if (res > 9 && res < 100)
 			{
 				_putchar(((res / 10) % 10) + '0');
 			}
 			_putchar((res % 10) + '0');
-			if (b != 9)
+			if (b != n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if ((a * (b + 1)) <= 9)
+				if (((a * (b + 1)) > 9) && ((a * (b + 1)) < 100))
 				{
+					_putchar(' ');
+				} else if ((a * (b + 1)) <= 9)
+				{
+					_putchar(' ');
 					_putchar(' ');
 				}
 			}
