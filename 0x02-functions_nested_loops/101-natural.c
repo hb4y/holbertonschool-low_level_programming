dc24972dd0d5eaa18c119ff0e6d9737be784dc25
hb4y <hugobaymon@gmail.com>
2019-09-24 23:44:57 +0000

    mult

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Description alphabet
+ * Return: Always 0 (Success)
+ **/
+
+void main(void)
+{
+	int n;
+	int m = 0;
+
+	for (n = 3; n < 1024; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+		{
+			m = m + n;
+		}
+	}
+	printf("%d\n", m);
+}
