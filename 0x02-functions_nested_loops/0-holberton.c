effb76e0c64cc22d5b71197f260449b27bc2f92c
hb4y <hugobaymon@gmail.com>
2019-09-24 12:44:28 +0000

    only 1 _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,15 +7,12 @@
 
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
-return (0);
+	int i;
+	char a[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\n'};
+
+	for (i = 0; i < (int)sizeof(a); i++)
+	{
+		_putchar(a[i]);
+	}
+	return (0);
 }
