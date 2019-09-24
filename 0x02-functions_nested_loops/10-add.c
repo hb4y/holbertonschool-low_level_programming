dbcb6ba23a750ed93852f6453ec4b6e4c9fd6c9a
hb4y <hugobaymon@gmail.com>
2019-09-24 19:06:34 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - function
+ * @n: take an int value
+ * @m: take an int value
+ *
+ * Return: addition n + m
+ * Cool...
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
