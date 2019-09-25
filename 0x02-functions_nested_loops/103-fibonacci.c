84549811d6d17c7e4ccc52a5a60aed590273b7d3
hb4y <hugobaymon@gmail.com>
2019-09-25 00:22:01 +0000

    fibo sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Description alphabet
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	long int one = 1;
+	long int two = 2;
+	long int hold;
+	long int sum = 0;
+
+	for (hold = 2; hold <= 4000000; hold = one + two)
+	{
+		if (hold % 2 == 0)
+		{
+			sum = sum + hold;
+		}
+		one = two;
+		two = hold;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
