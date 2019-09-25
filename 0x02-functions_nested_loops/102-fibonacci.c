4c45356cf08571614a5880e458590a42762ec1ad
hb4y <hugobaymon@gmail.com>
2019-09-25 00:08:20 +0000

    50 fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Description alphabet
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	long int one = 1;
+	long int two = 2;
+	long int hold;
+	int n;
+	
+	printf("1, 2, ");
+	for (n = 3; n <= 50; n++)
+	{
+		hold = one + two;
+
+		printf("%ld", hold);
+		if (n != 50)
+			printf(", ");
+		one = two;
+		two = hold;
+	}
+	printf("\n");
+
+	return (0);
+}
