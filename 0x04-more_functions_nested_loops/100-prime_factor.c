8e5779d2077f6889d8f56ff18ae21e675d89ce09
hb4y <hugobaymon@gmail.com>
2019-09-26 19:42:14 +0000

    style fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,21 +1,19 @@
 #include "holberton.h"
 
 /**
- * print_triangle - function
- * @n: size of triangle
- * Return: void
- * print a triangle of size n.
+ * main - function
+ * Return: 0
+ * largest prime factor.
  */
 int main(void)
 {
-	long unsigned int i;
-	long unsigned int n;
+	long i;
+	long n;
 
 	i = 612852475143;
-	
 	for (n = 3; n <= sqrt(i); n = n + 2)
 		while (i % n == 0)
 			i = i / n;
-	printf("%lu\n", i);
+	printf("%ld\n", i);
 	return (0);
 }
