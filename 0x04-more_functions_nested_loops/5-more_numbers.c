9c58ec4a7194ab258c5356d6c5cd063c9ecad5e8
hb4y <hugobaymon@gmail.com>
2019-09-26 18:26:47 +0000

    10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,14 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	
+
 	for (j = 1; j <= 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
 				_putchar(((i / 10) % 10) + 48);
-			_putchar((i % 10) + 48);		
+			_putchar((i % 10) + 48);
 		}
 		_putchar('\n');
 	}
