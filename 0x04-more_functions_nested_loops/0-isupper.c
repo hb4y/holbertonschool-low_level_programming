3a8df6e143c6a77011af43f8c76c48798cb5cd30
hb4y <hugobaymon@gmail.com>
2019-09-26 12:25:25 +0000

    first files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isupper - function
+ * Return: 1 if uppercase
+ * if not, 0 is returned.
+ */
+int _isupper(int c)
+{
+	int a;
+
+	if (c >= 65 && c <= 90)
+		a = 1;	
+	else
+		a = 0;
+	return (a);
+		
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _isupper(int c);
+int _putchar(char c);
+
+#endif /* HOLBERTON_H */
