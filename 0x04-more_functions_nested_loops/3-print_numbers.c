b7422abe15773dfceddd921dc439f378a0a44053
hb4y <hugobaymon@gmail.com>
2019-09-26 13:11:26 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - function
+ * Return: a * b
+ * multi two integers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
