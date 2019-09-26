d81b2223bddf92f48d09ae2802b5e3d005823b0c
hb4y <hugobaymon@gmail.com>
2019-09-26 12:30:28 +0000

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - function
+ * Return: 1 if digit
+ * if not, 0 is returned.
+ */
+int _isdigit(int c)
+{
+	int a;
+
+	if (c >= 48 && c <= 57)
+		a = 1;
+	else
+		a = 0;
+	return (a);
+
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,5 +3,6 @@
 
 int _isupper(int c);
 int _putchar(char c);
+int _isdigit(int c);
 
 #endif /* HOLBERTON_H */
