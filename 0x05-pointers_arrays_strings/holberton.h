17d35a0b19a5c607aed59ce45b1a13a1465cb2f2
hb4y <hugobaymon@gmail.com>
2019-09-30 20:46:55 +0000

    header

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,8 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif
