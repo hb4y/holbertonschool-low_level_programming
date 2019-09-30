96d7704772b11057dd52b3292e69ae57640d01ac
hb4y <hugobaymon@gmail.com>
2019-09-30 20:39:22 +0000

    len of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - find the len of a string.
+ * @s: char pointer
+ * Return: void.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	
+	i = 0;
+	while(*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
