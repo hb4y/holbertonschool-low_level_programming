2534c7a1f5a84814f9bc4fa279bf44300c1d8f23
hb4y <hugobaymon@gmail.com>
2019-10-01 22:42:42 +0000

    less pretty code for the checker

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,8 +11,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", a[i]);
-	printf("%d", a[n - 1]);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
 	printf("\n");
 }
