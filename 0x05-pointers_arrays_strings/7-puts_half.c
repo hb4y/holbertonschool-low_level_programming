a6bca2fa7a3a8342ad2e93e2bcca2a83177ef89c
hb4y <hugobaymon@gmail.com>
2019-10-01 21:07:33 +0000

    array index aproach

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,13 @@ void puts_half(char *str)
 {
 	int len, mid;
 
-	len = 0;
-	for (; *str != '\0'; str++)
-		len++;
-	mid = len / 2;
-	str = str - len;	
-	for (; str[mid] != '\0'; mid++)
+	for (len = 0; str[len] != '\0'; len++)
+	
+	if (len % 2 == 0) 	
+		mid = len / 2;
+	else
+		mid = (len + 1) / 2;
+	for (; str[mid] != '\0'; ++mid)
 		_putchar(str[mid]);
 	_putchar('\n');
 }
