c3541ab5dea241315c9013ef47f9ec8f72cfc9db
hb4y <hugobaymon@gmail.com>
2019-10-01 00:41:53 +0000

    working on 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * rev_string - print a string reverse.
+ * @s: string - strcpy
+ * Return: void.
+ */
+
+void rev_string(char *s)
+{
+	int len;
+	char tmp;
+
+	len = 0;
+	for (; *s != '\0'; s++)
+		len++;
+	for (; len > 0; len--)
+	{
+		s--;
+		a = tmp;
+	}
+
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -7,5 +8,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif
