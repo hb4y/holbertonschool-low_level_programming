a75630bff1d1ada751b7bfeec2fd2e939700aec1
hb4y <hugobaymon@gmail.com>
2019-10-03 16:40:37 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copy string.
+ * @dest: destination string
+ * @src: source string
+ * @n: n bytes
+ * Return: char*
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return dest;
+}
