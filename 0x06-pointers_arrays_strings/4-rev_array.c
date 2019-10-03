f92c511cf58d8572eebf3cc14903e2ca3af09160
hb4y <hugobaymon@gmail.com>
2019-10-03 22:09:05 +0000

    fix??

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse int array.
+ * @a: int - int pointer
+ * @n: number of elements
+ * Return: void.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int mid, i;
+	int tmp;
+
+	if (n % 2 != 0)
+		mid = (n / 2) + 1;
+	else
+		mid = n / 2;
+
+	n = n - 1;
+	for (i = 0; i < mid; i++)
+	{
+		tmp = *(a+i);
+		*(a+i) = *(a+n-i);
+		*(a+n-i) = tmp;
+	}
+}
