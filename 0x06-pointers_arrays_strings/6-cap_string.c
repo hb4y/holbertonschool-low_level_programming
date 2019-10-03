10f4a31344e14a1d6620ddad69ea56d5f799e344
hb4y <hugobaymon@gmail.com>
2019-10-03 23:17:13 +0000

    6 cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize all words.
+ * @c: init char
+ * Return: char pointer
+ */
+
+char *cap_string(char *c)
+{
+	int i, j;
+	char *separ = ",;.!?\"(){} ";
+
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; c[i] >= 'a' && c[i] <= 'z' && separ[j] != '\0'; j++)
+		{
+			if (i == 0)
+				c[i] = (int)c[i] - 32;
+			else if (c[i - 1] == separ[j])
+				c[i] = (int)c[i] - 32;
+			else if (c[i - 2] == '\\' && (c[i - 1] == 't' || c[i - 1] == 'n'))
+				c[i] = (int)c[i] - 32;
+		}
+	}
+	return (c);
+}
