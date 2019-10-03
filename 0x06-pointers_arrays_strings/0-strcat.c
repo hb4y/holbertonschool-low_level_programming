0a8df576be8e53bcbcd2e903a9af294a98b3ef86
hb4y <hugobaymon@gmail.com>
2019-10-03 18:03:18 +0000

    style fixes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,8 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (j = 0; dest[j] != '\0'; j++);
+	for (j = 0; dest[j] != '\0'; j++)
+		;
 
 	for (i = 0 ; src[i] != '\0' ; i++)
 		dest[j + i] = src[i];
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * _strcat - concat 2 strings.
+ * _strncat - concat 2 strings.
  * @dest: destination string
  * @src: source string
+ * @n: n bytes
  * Return: char*
  */
 
@@ -11,7 +12,8 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (j = 0; dest[j] != '\0'; j++);
+	for (j = 0; dest[j] != '\0'; j++)
+		;
 
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[j + i] = src[i];
