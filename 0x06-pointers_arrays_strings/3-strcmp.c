457efb5841b3b4cbca6d6e7d2f90372f01bde4fb
hb4y <hugobaymon@gmail.com>
2019-10-03 18:53:23 +0000

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,6 @@ int _strcmp(char *s1, char *s2)
 	int i, ret;
 
 	for (i = 0, ret = 0; ret == 0 && s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
 		ret = (int)(s1[i]) - (int)(s2[i]);
-		printf("{%d}\n", ret);
-	}
 	return (ret);
 }
