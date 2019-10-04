8e133f3e89c765dcd7f79cf6ea803b65c723974c
hb4y <hugobaymon@gmail.com>
2019-10-04 00:22:42 +0000

    headers

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,7 +1,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include <stdio.h>
+#include <stdlib.h>
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
