793ba86fab6ef390f6e7a676ecce30a10d88834e
hb4y <hugobaymon@gmail.com>
2019-10-07 20:02:46 +0000

    0 task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _memset - function
+ * @s: char pointer
+ * @b: char
+ * @n: n bytes
+ * Return: pointer to memo
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+		*(s + i) = b;
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* HOLBERTON_H */
