72bd1d70ca257d9c50c07dae2052acc290c57935
hb4y <hugobaymon@gmail.com>
2019-10-07 23:14:49 +0000

    print chess

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -2,10 +2,12 @@
 #define HOLBERTON_H
 #include <stdio.h>
 
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
 
 #endif /* HOLBERTON_H */
