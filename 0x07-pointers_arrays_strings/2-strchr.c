6ccfb92b252b722b70ccdbc835d1dd883b45349d
hb4y <hugobaymon@gmail.com>
2019-10-07 20:36:05 +0000

    2 strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function
+ * @s: char pointer
+ * @c: char
+ * Return: pointer to char
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (s + i);
+	return (NULL);
+}
