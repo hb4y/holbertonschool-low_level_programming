800f68a77954e14dace4a4427b6f24bcd4a3f585
hb4y <hugobaymon@gmail.com>
2019-10-07 22:57:11 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - function
+ * @s: char pointer
+ * @accept: accepted bytes
+ * Return: char pntr or null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (NULL);
+}
