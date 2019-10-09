f644323a75c819ea3bcde76af228d2b7ba67a392
hb4y <hugobaymon@gmail.com>
2019-10-09 13:46:39 +0000

    print recursive

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function
+ * @s: char pointer
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	} else
+		_putchar('\n');
+}
