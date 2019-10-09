63861419a58828b8d6f3ee8401796e01d35e51b5
hb4y <hugobaymon@gmail.com>
2019-10-09 14:53:33 +0000

    len recursive

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - function
+ * @s: char pointer
+ * Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+	
+	i = 0;
+	if (*s)
+	{
+		i++;
+		i = i + _strlen_recursion(s + 1);
+	}
+	return (i);
+}
