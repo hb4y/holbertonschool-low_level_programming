3939e468ce9a727f69f8fbdcbbf5cc26c6b1296d
hb4y <hugobaymon@gmail.com>
2019-10-09 19:33:01 +0000

    5 pleaseeeeee

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -8,5 +8,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt(int n, int count);
 
 #endif /* HOLBERTON_H */
