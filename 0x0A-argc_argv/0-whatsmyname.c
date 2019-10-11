d0edb40bc582580e227d2e56ec4a3b7f7e7f9b03
hb4y <hugobaymon@gmail.com>
2019-10-11 14:13:23 +0000

    0 commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/** main - function
+ * 
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+#endif
