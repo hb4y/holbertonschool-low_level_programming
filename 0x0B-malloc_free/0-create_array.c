8bb40eeba084cda3514041da6b26cb4195ea1a61
hb4y <hugobaymon@gmail.com>
2019-10-15 15:00:51 +0000

    allocate

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * create_array - function
+ * @size: size of array
+ * @c: char to fill the array
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(size * sizeof(c));
+
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
