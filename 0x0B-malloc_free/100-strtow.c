5364b1be0a9a8f373d1c48ae21b439ac4b7ad2a9
hb4y <hugobaymon@gmail.com>
2019-10-16 02:35:39 +0000

    advance

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * strtow - function
+ * @str: arg count
+ * Return: char **
+ */
+char **strtow(char *str)
+{
+	if (str == NULL || str == "")
+		return (NULL);
+	return (str);
+}
