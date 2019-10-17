28c2003aab54c3354eaf175fef3b92c44ab48a63
hb4y <hugobaymon@gmail.com>
2019-10-17 22:26:00 +0000

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,65 @@
+#include "holberton.h"
+
+/**
+ * _realloc - reallocates memory block
+ * @ptr: originnal ptr
+ * @old_size: old size
+ * @new_size: new size
+ * Return: reallocated ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *arr;
+
+	arr = ptr;
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+	{
+		ptr = malloc(new_size);
+		if (!ptr)
+			return (NULL);
+		return (ptr);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size > old_size)
+	{
+		_memcpy(arr, ptr, old_size);
+		free(ptr);
+		ptr = malloc(new_size);
+		_memcpy(ptr, arr, old_size);
+	}
+
+	if (new_size < old_size)
+	{
+		arr = _memcpy(arr, ptr, new_size);
+		free(ptr);
+		ptr = malloc(new_size);
+		_memcpy(ptr, arr, new_size);
+	}
+
+	return (ptr);
+}
+
+/**
+ * _memcpy - function
+ * @dest: destiny pntr
+ * @src: source pntr
+ * @n: n bytes
+ * Return: pointer to memo
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
+}
