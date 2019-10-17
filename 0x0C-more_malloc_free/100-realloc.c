be1437b4675bf416d0b077bf7cdfecc2da57f902
hb4y <hugobaymon@gmail.com>
2019-10-17 23:01:09 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,7 +11,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *arr;
 
-	arr = ptr;
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -29,11 +28,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size == old_size)
 		return (ptr);
-
 	arr = malloc(new_size);
 	if (!arr)
+	{
 		free(arr);
-
+		return (NULL);
+	}
 	if (new_size > old_size)
 	{
 		_memcpy(arr, ptr, old_size);
