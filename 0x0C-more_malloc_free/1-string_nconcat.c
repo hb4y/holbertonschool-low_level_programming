b749c2155c627c1283ac9d33de0018c5a613f875
hb4y <hugobaymon@gmail.com>
2019-10-17 18:21:32 +0000

    n > len

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,13 +22,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len2 = 1; s2[len2] != '\0'; len2++)
 		;
 
+	if (len2 <= n)
+		n = len2;
 	arr = malloc((len + n + 1) * sizeof(char));
 
 	if (!arr)
 		return (NULL);
-	if (len2 <= n)
-		n = len2;
-	for (i = 0; i < len; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		arr[i] = s1[i];
 	for (j = 0; j < n; j++, i++)
 		arr[i] = s2[j];
