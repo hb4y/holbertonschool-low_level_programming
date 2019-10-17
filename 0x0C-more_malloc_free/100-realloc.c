1009d56872b21c3f0480dd6b9ca3380f185a835b
hb4y <hugobaymon@gmail.com>
2019-10-17 22:31:58 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,10 +20,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (!ptr)
 	{
-		free(ptr);
 		ptr = malloc(new_size);
 		if (!ptr)
+		{
+			free(ptr);
 			return (NULL);
+		}
 		return (ptr);
 	}
 
