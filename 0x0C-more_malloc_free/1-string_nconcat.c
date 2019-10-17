13af1a8bd6740764ba908b7c0b0b0c823b5f431d
hb4y <hugobaymon@gmail.com>
2019-10-17 17:55:54 +0000

    1 task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * string_nconcat - concat 2 strings unitl n bytes
+ * @s1: char *
+ * @s2: char *
+ * @n: n bytes of s2 to concat
+ * Return: char pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *arr;
+	int len, i;
+	unsigned int j, len2;
+
+	for (len = 1; s1[len] != '\0'; len++)
+		;
+	for (len2 = 1; s2[len2] != '\0'; len2++)
+		;
+
+	arr = malloc((len + n + 1) * sizeof(char));
+
+	if (!arr)
+		return (NULL);
+	if (len2 <= n)
+		n = len2;
+	for (i = 0; i < len; i++)
+		arr[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		arr[i] = s2[j];
+	arr[i] = '\0';
+	return (arr);
+}
