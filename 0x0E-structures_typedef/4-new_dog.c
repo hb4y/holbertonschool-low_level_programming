9ba7c11b0db4bccacf159ee53d69e61d773f3c3b
hb4y <hugobaymon@gmail.com>
2019-10-21 23:17:49 +0000

    create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,66 @@
+#include "dog.h"
+
+/**
+ * new_dog - create a new dog
+ * @name: char pntr
+ * @age: float number
+ * @owner: char pntr
+ * Return: dog struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *bestia;
+	char *cpy_name, *cpy_owner;
+	int len;
+	
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	
+	bestia  = malloc(sizeof(dog_t));
+	if (!bestia)
+		return (NULL);
+
+	for (len = 1; name[len] != '\0'; len++)
+		;
+	cpy_name = malloc(len * sizeof(char));
+	if (!cpy_name)
+	{
+		free(bestia);
+		return (NULL);
+	}
+	_strncpy(cpy_name, name, len);
+
+	for (len = 1; owner[len] != '\0'; len++)
+		;
+	cpy_owner = malloc(len * sizeof(char));
+	if (!cpy_owner)
+	{
+		free(bestia);
+		return (NULL);
+	}
+	_strncpy(cpy_owner, owner, len);
+
+	bestia->name = cpy_name;
+	bestia->age = age;
+	bestia->owner = cpy_owner;
+	
+	return (bestia);
+}
+
+/**
+ * _strncpy - copy string.
+ * @dest: destination string
+ * @src: source string
+ * @n: n bytes
+ * Return: char*
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,7 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif /* DOG_H */
