b9ba8cebe5906fad0562f9be0a4d70eb5ca1b460
hb4y <hugobaymon@gmail.com>
2019-10-22 01:57:04 +0000

    who let the dogs out?? woof woof wofgit add dog.h 4-new_dog.c !

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
