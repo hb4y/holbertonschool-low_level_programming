d06e1c629dd03f53c71a6e0d3c79c730dc3df5c2
hb4y <hugobaymon@gmail.com>
2019-10-23 21:52:03 +0000

    adv

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count
+ * @argv: argument array
+ * Return: Always 0.
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (argv[1] < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < atoi(argv[1]); i++)
+	{
+		printf("%x", (*(char *)main) + i);
+
+		if (i < (atoi(argv[1]) - 1))
+			printf(" ");
+	}
+
+	printf("\n");
+
+
+	return (1);
+}
