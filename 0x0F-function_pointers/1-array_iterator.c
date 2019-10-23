73f2932b53aec92b9c13aa4ec2391d0aec792d57
hb4y <hugobaymon@gmail.com>
2019-10-23 17:36:52 +0000

    check if null

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
