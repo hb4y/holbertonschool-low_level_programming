efe5ffc3e2f44e74e5a67711513e2b20514542f7
hb4y <hugobaymon@gmail.com>
2019-10-25 21:48:45 +0000

    variadic

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - function
+ * @n: int parameters to sum 
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int sum, i;
+
+	sum = 0;
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - function
+ * @separator: char pntr separator
+ * @n: int :arguments
+ * Return: sum
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		printf("%s", separator);
+	}
+	printf("%d\n", va_arg(valist, int));
+
+	va_end(valist);
+}
