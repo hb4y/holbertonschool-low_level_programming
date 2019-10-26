9bebe90a457bace96ffbbd26b9337f79cbb7979b
hb4y <hugobaymon@gmail.com>
2019-10-26 19:01:37 +0000

    seg fault fix in 0 args

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,13 +12,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(valist, n);
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
-		if (separator)
+		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
-	printf("%d", va_arg(valist, int));
 
 	va_end(valist);
 
