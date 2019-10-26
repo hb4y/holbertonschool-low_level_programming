c5aef3a367abd6e380a71cf13e9393a0ec074fd3
hb4y <hugobaymon@gmail.com>
2019-10-26 22:46:51 +0000

    header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,12 +12,16 @@
 typedef struct op
 {
 	char *op;
-	int (*f)(int a, int b);
+	void (*f)(va_list);
 } op_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/*void p_char_pntr(char *s);
+void p_float(float f);
+void p_int(int i);
+void p_char(char c);*/
 
 #endif /* VARIADIC_H */
