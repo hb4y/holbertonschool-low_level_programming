d0de72871d575a49e41a74b03614f559492167e9
hb4y <hugobaymon@gmail.com>
2019-10-27 00:44:24 +0000

    fix requirements

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,12 +1,15 @@
-	global	main
-	extern	puts
+section	.data
+	msg db "Hello, Holberton", 0x0a
+	size equ $ - msg
 
-	section	.text
-
+section .text
+	global main
 main:
-	mov	rdi, message
-	call	puts
-	ret
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, size
+	int 0x80
+	mov eax, 0
+	int 0x80
 
-message:
-	db	"Hello, Holberton", 0
