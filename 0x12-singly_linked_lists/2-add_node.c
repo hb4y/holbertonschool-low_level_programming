8fe6529193797b9bca5d5d4fcc0394351b31f788
hb4y <hugobaymon@gmail.com>
2019-10-31 21:02:49 +0000

    add node at beg

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_node- function that adds a node at beginning
+ * @h: list
+ * Return: number of nodes
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	len = 0;
+
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	if (!(new->str))
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len]; len++)
+		;
+
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	return (NULL);
+}
