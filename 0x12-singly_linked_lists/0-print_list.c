5bf99975e11b79d8dcdf4f41dfaae7a6282c433a
hb4y <hugobaymon@gmail.com>
2019-10-31 16:04:05 +0000

    0 task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - function
+ * @h: list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	if (!(*h).str)
+	{
+		printf("[0] (nil)\n");
+		i++;
+	}
+	else if ((*h).str)
+	{
+		printf("[%u] %s\n", (*h).len, (*h).str);
+		i++;
+	}
+
+	if((*h).next)
+		i += print_list((*h).next);
+
+	return (i);
+}
