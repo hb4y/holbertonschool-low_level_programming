e25eb491d456096655617978ed5e595d98b29415
hb4y <hugobaymon@gmail.com>
2019-11-01 00:50:58 +0000

    1 adv && betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_list - function
- * @h: list
- * Return: number of nodes
+ * before_main - execute before main
+ * Return: void
  */
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
