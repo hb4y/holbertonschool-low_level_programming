8118eb3455bddf7ad534f17e8f29d698f1b69adc
hb4y <hugobaymon@gmail.com>
2019-11-01 00:38:07 +0000

    free all

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_list - free the mem of the list
+ * @head: list_t address
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	if (head->next)
+		free_list(head->next);
+
+	free(head->str);
+	free(head);
+}
