bf3387e66d9791d60888c36d5e8abe5f647f620f
hb4y <hugobaymon@gmail.com>
2019-11-01 00:49:48 +0000

    1 adv && betty style

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_list - function
+ * @h: list
+ * Return: number of nodes
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,6 +20,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+void before_main(void) __attribute__ ((constructor));
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
