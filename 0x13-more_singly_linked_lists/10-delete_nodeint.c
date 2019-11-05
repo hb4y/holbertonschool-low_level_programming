4b991974e89a9f11b63a375172c2ea7232364121
hb4y <hugobaymon@gmail.com>
2019-11-05 20:07:56 +0000

    task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete node
+ * @head: listint_t address
+ * @index: index to insert
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *tmp;
+
+	if (!head || !(*head))
+		return (-1);
+
+	if (index == 0)
+	{
+		if (!((*head)->next))
+		{
+			free(*head);
+			*head = NULL;
+			return (1);
+		}
+		free(*head);
+		*head = (*head)->next;
+		return (1);
+	}
+
+	tmp = *head;
+
+	for (i = 0; tmp && i < index; i++)
+	{
+		if (i == index - 1)
+		{
+			if (tmp->next)
+			{
+				tmp->next = (tmp->next)->next;
+				return (1);
+			}
+		}
+		tmp = tmp->next;
+	}
+	return (-1);
+}
