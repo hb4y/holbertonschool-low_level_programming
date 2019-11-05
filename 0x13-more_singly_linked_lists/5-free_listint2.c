336bdda8d6d75622d10fc83ef2f86d3a4a265cd5
hb4y <hugobaymon@gmail.com>
2019-11-05 15:57:09 +0000

    5 head null caes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,13 @@
  */
 void free_listint2(listint_t **head)
 {
+	if (!head)
+		return;
+
 	if ((*head)->next)
 		free_listint2(&(*head)->next);
 
 	free((*head));
+
 	*head = NULL;
-	head = NULL;
 }
