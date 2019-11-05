b499b513f3ba3c1d69dc40851eedbc7369d47090
hb4y <hugobaymon@gmail.com>
2019-11-05 15:11:34 +0000

    5 task, free an null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free the mem of the list && set head to NULL
+ * @head: listint_t address
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	if (!(*head))
+		return;
+
+	if ((*head)->next)
+		free_listint2(&(*head)->next);
+
+	free((*head));
+	*head = NULL;
+}
