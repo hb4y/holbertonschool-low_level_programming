653615dd9ee837a13879022880c7f35e35d433de
hb4y <hugobaymon@gmail.com>
2019-11-05 15:21:28 +0000

    5 task NULL node entry

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,9 +7,12 @@
  */
 void free_listint2(listint_t **head)
 {
-	if ((*head)->next)
-		free_listint2(&(*head)->next);
+	if (!(*head))
+	{
+		if ((*head)->next)
+			free_listint2(&(*head)->next);
 
-	free((*head));
-	*head = NULL;
+		free((*head));
+		*head = NULL;
+	}
 }
