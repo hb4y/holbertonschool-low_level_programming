f9accd6dff906805d000c68844fc098c548e54e6
hb4y <hugobaymon@gmail.com>
2019-11-07 19:55:44 +0000

    0 task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * _pow_recursion  - function
+ * @x: int base
+ * @y: int power
+ * Return: x power y
+ */
+int _pow_recursion(int x, int y)
+{
+	int pow;
+
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	pow = 1;
+	y--;
+	pow = x * _pow_recursion(x, y);
+	return (pow);
+}
+
+
+/**
+ * binary_to_uint - function
+ * @b: char array of 0 and 1
+ * Return: converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, j;
+	unsigned int uint;
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	uint = 0;
+	i--;
+
+	for (j = 0; i >= 0; i--, j++)
+	{
+		if (b[i] == '1')
+			uint += _pow_recursion(2, j);
+	}
+
+	return (uint);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+unsigned int binary_to_uint(const char *b);
+
+
+#endif /* HOLBERTON_H */
