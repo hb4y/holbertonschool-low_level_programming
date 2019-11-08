19bb593f8b739b7d11ecb651c1a7b88c6a53e17d
hb4y <hugobaymon@gmail.com>
2019-11-08 00:06:04 +0000

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * clear_bit - function
+ * @n: pointer to UL
+ * @index: nth bit to set to 1
+ * Return: 1 if work or -1 if not
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask;
+
+	if (index > 63)
+		return (-1);
+
+	mask = 1UL << index;
+
+	*n = *n & mask;
+
+	return (1);
+}
