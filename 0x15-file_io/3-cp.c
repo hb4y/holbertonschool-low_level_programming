993818bfc005299d9ee17054890a0b822169f44b
hb4y <hugobaymon@gmail.com>
2019-11-12 21:45:23 +0000

    fix error msg (98)

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	}
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[1]);
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
