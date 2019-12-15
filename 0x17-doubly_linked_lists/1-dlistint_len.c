ed2d4267375ae264fa2a91c996a862e4b91828f6
hb4y <hugobaymon@gmail.com>
2019-12-15 23:00:48 +0000

    number of elements in dlist

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - function
+ * @h: list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	if (!h)
+		return (0);
+
+	while ((*h).prev)
+		h = (*h).prev;
+
+	for (i = 1; (*h).next; h = (*h).next, i++)
+		;
+
+	return (i);
+}
