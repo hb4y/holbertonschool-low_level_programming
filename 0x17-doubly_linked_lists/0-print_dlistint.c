f6038e2e6c9cd3b61a156ea6894bb3b25c770fab
hb4y <hugobaymon@gmail.com>
2019-12-15 22:26:34 +0000

    print double list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_dlistint - function
+ * @h: list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	i = 0;
+
+	if (!h)
+		return (0);
+	if ((*h).n)
+	{
+		printf("%i\n", (*h).n);
+		i++;
+	}
+
+	if ((*h).next)
+		i += print_dlistint((*h).next);
+
+	return (i);
+}
