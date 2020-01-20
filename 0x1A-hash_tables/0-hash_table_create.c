5970dc8ecc42ea0fb29dd10e1bb0ec527f3b9dda
hb4y <hugobaymon@gmail.com>
2020-01-20 05:38:28 +0000

    free

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,6 +14,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t **));
 	if (!(new_table->array))
+	{
+		free(new_table);
 		return (NULL);
+	}
 	return (new_table);
 }
