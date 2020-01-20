9486a75e1bfb82e31fc7547d76935575761e06e1
hb4y <hugobaymon@gmail.com>
2020-01-20 01:59:55 +0000

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,16 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create -
- *
- * Return: 
+ * hash_table_create - create a hash table
+ * @size: size
+ * Return: hash_table pointer
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
+	if (!new_table)
+		return (NULL);
+	new_table->size = size;
+	return (new_table);
 }
