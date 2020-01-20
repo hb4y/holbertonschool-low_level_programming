a9485fe28283b1343ed78acaa1bc7d310ab73165
hb4y <hugobaymon@gmail.com>
2020-01-20 06:09:47 +0000

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get -
- *
- * Return: 
+ * hash_table_get - function
+ * @ht: hash table
+ * @key: key
+ * Return: value of hash
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int i;
+	hash_node_t *aux;
 
+	if ((!ht) || (!key) || (!(*key)))
+		return (NULL);
+
+	i = key_index((const unsigned char *)key, ht->size);
+	aux = ht->array[i];
+
+	for (; aux; aux = aux->next)
+	{
+		if (strcmp(aux->key, key) == 0)
+			return (aux->value);
+	}
+	return (NULL);
 }
