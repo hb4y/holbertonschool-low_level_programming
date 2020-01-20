8b3d482954b9ee327a07256c1429e47e701455df
hb4y <hugobaymon@gmail.com>
2020-01-20 05:49:51 +0000

    task 3

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * key_index -
- *
- * Return: 
+ * key_index - function
+ * @key: key
+ * @size: size
+ * Return: index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	unsigned long int aux;
 
+	aux = hash_djb2(key);
+	return (aux % size);
 }
