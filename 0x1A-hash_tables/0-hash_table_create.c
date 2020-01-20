19da893ade8f8d287418286b9a98846b9ae04ad4
hb4y <hugobaymon@gmail.com>
2020-01-20 05:42:39 +0000

    change malloc to calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,12 +7,12 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	hash_table_t *new_table = calloc(sizeof(hash_table_t), 1);
 
-	if (!new_table || !size)
+	if ((!new_table) || (!size))
 		return (NULL);
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t **));
+	new_table->array = calloc(size, sizeof(hash_node_t **));
 	if (!(new_table->array))
 	{
 		free(new_table);
