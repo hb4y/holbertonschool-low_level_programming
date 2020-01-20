17a78c15557f7728d11879907972dfd8e9c1a2c6
hb4y <hugobaymon@gmail.com>
2020-01-20 06:23:02 +0000

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete -
- *
- * Return: 
+ * hash_table_delete - func
+ * @ht: hash table
+ * Return: void
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *tmp;
+	hash_node_t *aux;
 
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+
+		for (; tmp; tmp = tmp->next)
+		{
+			aux = tmp;
+			free(tmp->key);
+			free(tmp->value);
+			free(aux);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
