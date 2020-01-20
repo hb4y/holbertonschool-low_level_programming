58cc0e602996a5e7d9506f8dbe0dcb4e3aac5db0
hb4y <hugobaymon@gmail.com>
2020-01-20 05:37:24 +0000

    malloc fails

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
-	if (!size)
-		return (NULL);
-	if (!new_table)
+	if (!new_table || !size)
 		return (NULL);
 	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t **));
+	if (!(new_table->array))
+		return (NULL);
 	return (new_table);
 }
