f015a817d0429235dd737ab0836d420b1a930376
hb4y <hugobaymon@gmail.com>
2020-01-20 06:17:47 +0000

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
- *
- * Return: 
+ * hash_table_print - function
+ * @ht: hash table
+ * Return: just print
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *aux;
+	int ini = 0;
 
+	if (!ht)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		aux = ht->array[i];
+		for (; aux; aux = aux->next)
+		{
+			if (ini)
+				printf(", ");
+			printf("'%s': '%s'", aux->key, aux->value);
+			ini = 1;
+		}
+	}
+	printf("}\n");
 }
