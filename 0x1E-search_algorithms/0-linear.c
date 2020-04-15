6dba90556936bf133feaf3ade285daf032c662dd
hb4y <hugobaymon@gmail.com>
2020-04-15 14:53:16 +0000

    [task 0] Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/*
+ * linear_search - Implement linear search
+ * @array: integer array
+ * @size: size of the array
+ * @value: value to search for
+ * Return: Index of the value in the array
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+	
+	for (i = 0; i < size; i++)
+	{
+		if (*(array + i) != value)
+			printf("Value checked array[%d] = [%d]\n", (int) i, *(array + i));
+		else
+			return (i);
+	}
+
+	return (-1);
+
+}
